b396a9e4259900cdd8addb41357b4d46cfa59328
virgilxu123 <virgiliosio2016@gmail.com>
2022-12-06 14:13:49 +0800

    first commit

---
+++ index.php
@@ -0,0 +1,63 @@
+<?php include 'headr.php'; ?>
+
+<!-- Home -->
+    <div id="home">
+        <div class="left">
+            <div class="container">
+                <p class="left-content1">Welcome to College of Information Technology Education</p>
+                <p class="left-content2">Student Hub</p>
+                <p class="left-content3">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Itaque numquam consequuntur velit quaerat fugit nisi sunt aspernatur omnis dignissimos debitis quidem ipsa quod quisquam eligendi, corrupti, exercitationem aut ut perspiciatis.</p>
+                <button class="left-content4" id="myBtn">Register</button>
+            </div>
+        </div>
+        <div class="right">
+            <img src="images\student.png" alt="">
+        </div>
+    </div>
+    <!-- The Modal -->
+    <div id="myModal" class="modal">
+        <!-- Modal content -->
+        <div class="modal-content">
+            <span class="close">&times;</span>
+            <form action="" method="post" class="form">
+                <input type="text" placeholder="First name">
+                <input type="text" placeholder="Last name">
+                <input type="text" placeholder="Username">
+                <input type="password" placeholder="Password">
+                <input type="password" placeholder="Confirm password">
+                <input type="button" value="Register">
+            </form>
+        </div>
+    </div>
+    <!-- The Modal -->
+<!-- Home -->
+<!-- Events -->
+    <h1 class="h1">EVENTS</h1>
+    <div id="events">
+        <div class="left-events">
+            <div class="cover">
+                <h1 class="p3">CITE Day and IT Lympics Activities</h1>
+                <h4 class="p4">"Creative Minds Generates Technological Innovations."</h4>
+            </div>        
+        </div>
+        <div class="act-container">
+            <div class="act">
+                <img src="images\4.jpg" alt="">
+                <p class="p1">Day 1 Activity</p>
+                <p class="p2">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Minima architecto sequi rerum impedit fugit veritatis.</p>  
+            </div>
+            <div class="act">
+                <img src="images\laboratory1.jpg" alt=""> 
+                <p class="p1">Day 2 Activity</p>
+                <p class="p2">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Minima architecto sequi rerum impedit fugit veritatis.</p>             
+            </div>
+            <div class="act">
+                <img src="images\2.jpg" alt="">
+                <p class="p1">Day 3 Activity</p>
+                <p class="p2">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Minima architecto sequi rerum impedit fugit veritatis.</p>  
+            </div>
+        </div>
+    </div>
+<!-- Events -->
+    <script src="script\script.js"></script>
+<?php include 'footer.php'; ?>
